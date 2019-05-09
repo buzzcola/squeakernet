9bea5b3731dde87a78e749c9b0a43d8031cf2bba
buzzcola <orr.james@gmail.com>
2019-05-08 22:13:33 -0400

    Kibble contingency plan activated.

--- squeakernet/feeder.py
+++ squeakernet/feeder.py
@@ -19,6 +19,7 @@ pwm_still = config.getint("dispenser", "pwm_still")
 crank_speed = config.getint("dispenser", "crank_speed")
 grams_per_second = config.getfloat("dispenser", "grams_per_second")
 desired_grams = config.getfloat("dispenser","desired_grams")
+retry_threshold = config.getfloat("dispenser","retry_threshold")
 crank_time = desired_grams / grams_per_second
 servo_pin = config.getint("dispenser", "servo_pin")
 
@@ -36,8 +37,19 @@ def feed_the_cats():
     go(pwm_clockwise)
     weight_after = scale.get_weight()
     
-    if weight_after <> None and weight_before <> None:
+    if weight_after <> None and weight_before <> None:        
         dispensed = weight_after - weight_before
+        print 'Kibbles dispensed: %s grams.' % dispensed
+        shortfall = desired_grams - dispensed
+        if shortfall >= retry_threshold:
+            print 'Shortall of %s grams - kibble contingency activated.' % shortfall
+            weight_before = scale.get_weight()
+            speech.say("Inadquate kibble flow detected. Activating kibble contingency plan.")
+            go(pwm_clockwise, shortfall / grams_per_second)
+            weight_after = scale.get_weight()
+            if weight_after <> None and weight_before <> None: 
+                dispensed += (weight_after - weight_before)
+
         speech.say('I have dispensed %.1f grams of kibbles for you to enjoy. You are very good cats and I love you.' % dispensed)
         db.write_log(LogCategory.FEED, 'Turned crank for %.1f seconds, dispensing %.1fg of kibbles.' % (crank_time, dispensed), dispensed)
         scale.log_weight(weight_after)
@@ -69,8 +81,8 @@ def initialize_servo():
     wiringpi.pwmSetClock(192)
     wiringpi.pwmSetRange(2000)
     
-def go(direction):
+def go(direction, amount = crank_time):
     # turn the crank.
     wiringpi.pwmWrite(servo_pin, direction)
-    time.sleep(crank_time)
+    time.sleep(amount)
     wiringpi.pwmWrite(servo_pin, pwm_still)
